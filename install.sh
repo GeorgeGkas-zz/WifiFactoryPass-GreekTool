33f368d14e87a90891cd0feb1cb97c9938b2d67f
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-08-24 21:59:09 +0300

    Update to Version 0.2.1

--- install.sh
+++ install.sh
@@ -1,45 +1,5 @@
  #!/bin/bash 
 
-function install_pixiewps(){
-echo "We are going to install pixiewps"
-wget https://github.com/wiire/pixiewps/archive/master.zip && unzip master.zip
-
-cd pixiewps-master
-cd src
-make
-sudo make install
-
-cd ../
-rm master.zip 
-echo "Successfully installed pixiewps"
-}
-
-function install_reaver_fork(){
-echo "We are going to install reaver 1.4 with pixiewps support"
-wget https://github.com/t6x/reaver-wps-fork-t6x/archive/master.zip && unzip master.zip
-
-cd reaver-wps-fork-t6x-master
-cd src
-./configure
-make
-sudo make install
-
-cd ../
-rm master.zip
-echo "Successfully installed reaver 1.4 with pixiewps support"
-}
-
-function install_wifitools(user){
-echo "We are going to install WifiFactoryPass-GreekTool now! "
-sudo cp -r  wifitools ../../home/user/.wifitools
-chmod 777 wifitools.sh
-sudo cp wifitools.sh ../../usr/bin/
-
-cd ../
-rm master.zip
-echo "Successfully installed WifiFactoryPass-GreekTool! YEAHHHHHHHHH"
-}
-
 user ="$(whoami)"
 
 echo "Welcome to installation of WifiFactoryPass-GreekTool"
@@ -51,8 +11,49 @@ sudo apt-get -y install build-essential libpcap-dev sqlite3 libsqlite3-dev libss
 echo "Now we are going to install aircrack-ng"
 sudo apt-get -y install aircrack-ng
 
-(cd /tmp && install_pixiewps() && install_reaver_fork())
-install_wifitools(user)
+cd /tmp
+
+echo "We are going to install pixiewps"
+wget https://github.com/wiire/pixiewps/archive/master.zip && unzip master.zip
+
+cd pixiewps-master
+cd src
+make
+sudo make install
+
+cd ../
+cd ../
+rm master.zip 
+echo "Successfully installed pixiewps"
+
+echo "We are going to install reaver 1.4 with pixiewps support"
+wget https://github.com/t6x/reaver-wps-fork-t6x/archive/master.zip && unzip master.zip
+
+cd reaver-wps-fork-t6x-master
+cd src
+./configure
+make
+sudo make install
+
+cd ../
+cd ../
+cd ../
+rm master.zip
+echo "Successfully installed reaver 1.4 with pixiewps support"
+
+echo "We are going to install WifiFactoryPass-GreekTool now! "
+cd ~
+cd Downloads
+cd WifiFactoryPass-GreekTool
+
+sudo cp -r  wifitools ../../.wifitools
+chmod 777 wifitools.sh
+sudo cp wifitools.sh /usr/bin/
+
+cd ../
+rm master.zip
+echo "Successfully installed WifiFactoryPass-GreekTool! YEAHHHHHHHHH"
+
 
 echo "Finished installation"
 echo "To run the program run wifitools.sh with root privelages"
